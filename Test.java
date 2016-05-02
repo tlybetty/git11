2fa435742196ac47d328a1892003ff513a1a76df
LY T <729138608@qq.com>
2016-05-02 21:25:07 +0800

    '再次修改‘

--- Test.java
+++ Test.java
@@ -4,8 +4,21 @@ public class Test
 {
 	public static void main(String [] args){
 		Test test=new Test();
-		test.test2();
+		test.test3();
 	
+	}
+	public void test3(){
+		int[]arr=new int[]{9,3,3,4};
+		int[]arr2=new int[8];
+		System.arraycopy(arr,2,arr2,0,arr.length-2);
+		showArray(arr);
+		showArray(arr2);	
+	}
+	public void showArray(int[]arr){
+		for(int i :arr){
+			System.out.print(i+",");
+	  }
+			System.out.println();
 	}
 	public void test2(){
 		String info="1001#terry#12";
